f01f056968d553f21be7f89ee5f9b2eaa568e290
Zyrephus <wilsonchen315@gmail.com>
2024-05-11 13:06:25 -0400

    Finished sharing state with components

--- react-app/src/App.tsx
+++ react-app/src/App.tsx
@@ -1,9 +1,20 @@
-import Button from "./components/Button";
+import { useState } from "react";
+import { BsFillCalendarFill } from 'react-icons/bs';
+import Button from './components/Button/Button';
+import Like from './components/Like';
+import NavBar from './components/NavBar';
+import Cart from './components/Cart';
 
 function App() {
+  const [cartItems, setCartItems] = useState(['Product1', 'Product2']);
+
   return (
     <div>
-      <Button onClick={() => console.log('Clicked')}>My Button</Button>
+      <NavBar cartItemsCount={cartItems.length} />
+      <Cart cartItems={cartItems} onClear={()=> setCartItems([])} />
+      <BsFillCalendarFill color="red" size="40"/>
+      <Button onClick={() => {console.log("Button clicked!")}}>My Button</Button>
+      <Like onClick={() => console.log("Like clicked!")}></Like>
     </div>
   );
 }
