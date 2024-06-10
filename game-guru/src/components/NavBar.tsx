1f74c16f3b5bf119a7b83c119122880c470b3d80
Zyrephus <wilsonchen315@gmail.com>
2024-06-10 17:02:09 -0400

    Added dynamic page heading
    Also changed logo

--- game-guru/src/components/NavBar.tsx
+++ game-guru/src/components/NavBar.tsx
@@ -10,7 +10,7 @@ interface Props {
 const NavBar = ({ onSearch }: Props) => {
   return (
     <HStack padding='10px'>
-      <Image src={logo} boxSize={"60px"}/>
+      <Image src={logo} boxSize={"60px"} borderRadius={"10px"}/>
       <SearchInput onSearch={onSearch} />
       <ColorModeSwitch />
     </HStack>
