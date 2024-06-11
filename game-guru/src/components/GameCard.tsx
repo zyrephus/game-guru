d0e855c68ae2ff104fb3373c57dcb8671dd39592
Zyrephus <wilsonchen315@gmail.com>
2024-06-11 10:56:17 -0400

    Cleaned up game cards

--- game-guru/src/components/GameCard.tsx
+++ game-guru/src/components/GameCard.tsx
@@ -14,13 +14,13 @@ const GameCard = ({ game }: Props) => {
       <Image src={getCroppedImageUrl(game.background_image)} />
       <CardBody>
         {/* Title */}
-        <Heading fontSize="2xl">{game.name}</Heading>
-        <HStack justifyContent='space-between'>
+        <HStack justifyContent='space-between' marginBottom={3}>
           {/* Platforms */}
           <PlatformIconList platforms={game.parent_platforms.map((p) => p.platform)} />
           {/* Critic Score */}
           <CriticScore score={game.metacritic} />
         </HStack>
+        <Heading fontSize="2xl">{game.name}</Heading>
       </CardBody>
     </Card>
   );
