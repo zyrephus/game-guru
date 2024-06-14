0e10d55d25c319f5cc846659c398d8a1dd992205
Zyrephus <wilsonchen315@gmail.com>
2024-06-14 15:12:09 -0400

    Changed logo

--- src/components/NavBar.tsx
+++ src/components/NavBar.tsx
@@ -10,7 +10,7 @@ interface Props {
 const NavBar = ({ onSearch }: Props) => {
   return (
     <HStack padding='10px'>
-      <Image src={logo} boxSize={"60px"} borderRadius={"10px"}/>
+      <Image src={logo} boxSize={"60px"} borderRadius={"10px"} boxShadow="md"/>
       <SearchInput onSearch={onSearch} />
       <ColorModeSwitch />
     </HStack>
