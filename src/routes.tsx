fc987debe6f30a63910cc3c29155b4e0a98eb0cf
Zyrephus <wilsonchen315@gmail.com>
2024-07-23 12:53:06 -0400

    Added error page

--- src/routes.tsx
+++ src/routes.tsx
@@ -2,11 +2,13 @@ import { createBrowserRouter } from "react-router-dom";
 import Homepage from "./pages/Homepage";
 import Layout from "./pages/Layout";
 import GameDetailPage from "./pages/GameDetailPage";
+import ErrorPage from "./pages/ErrorPage";
 
 const router = createBrowserRouter([
   {
     path: "/",
     element: <Layout />,
+    errorElement: <ErrorPage />,
     children: [
       { index: true, element: <Homepage /> },
       { path: "games/:id", element: <GameDetailPage /> },
