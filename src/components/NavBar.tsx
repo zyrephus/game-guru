1af79530b374a9e083a93302e542d8cb1524279a
Zyrephus <wilsonchen315@gmail.com>
2024-07-26 19:54:38 -0400

    Add hover effect to logo button in NavBar

--- src/components/NavBar.tsx
+++ src/components/NavBar.tsx
@@ -1,16 +1,32 @@
-import { HStack, Image } from '@chakra-ui/react';
-import logo from '../assets/logo.webp';
-import ColorModeSwitch from './ColorModeSwitch';
-import SearchInput from './SearchInput';
+import { Button, HStack, Image } from "@chakra-ui/react";
+import { Link } from "react-router-dom";
+import logo from "../assets/logo.webp";
+import ColorModeSwitch from "./ColorModeSwitch";
+import SearchInput from "./SearchInput";
 
 const NavBar = () => {
   return (
-    <HStack padding='10px'>
-      <Image src={logo} boxSize={"60px"} borderRadius={"10px"} boxShadow="md"/>
+    <HStack padding="10px">
+      {/* Logo is a button that takes the user to the homescreen */}
+      <Link to="/">
+        <Button
+          variant="unstyled"
+          w="60px"
+          h="60px"
+          minW="60px" // Ensures the button doesn't shrink below the image size
+          minH="60px"
+          _hover={{
+            transform: "scale(1.05)",
+          }}
+          transition="transform .15s ease-in"
+        >
+          <Image src={logo} borderRadius={"10px"} boxShadow="md" />
+        </Button>
+      </Link>
       <SearchInput />
       <ColorModeSwitch />
     </HStack>
   );
-}
+};
 
-export default NavBar
+export default NavBar;
