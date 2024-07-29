836684edebba7fb72b42a24a6e5b99911f087034
Zyrephus <wilsonchen315@gmail.com>
2024-07-29 19:36:27 -0400

    Build game trailer on game page

---
+++ src/components/GameTrailer.tsx
@@ -0,0 +1,21 @@
+import useTrailers from "../hooks/useTrailers";
+
+interface Props {
+  gameId: number;
+}
+
+const GameTrailer = ({ gameId }: Props) => {
+  const { data, error, isLoading } = useTrailers(gameId);
+  console.log(data)
+
+  if (isLoading) return null;
+
+  if (error) throw error;
+
+  const first = data?.results[0];
+  return first ? (
+    <video src={first.data[480]} poster={first.preview} controls className="w-full h-auto" />
+  ) : null;
+};
+
+export default GameTrailer;
