76f97ffb8d3ac32e6fb2f8ddee45b4a2613be715
Zyrephus <wilsonchen315@gmail.com>
2024-07-31 09:55:48 -0400

    Add N/A rating for games with no rating

--- src/components/CriticScore.tsx
+++ src/components/CriticScore.tsx
@@ -5,10 +5,10 @@ interface Props {
 }
 
 const CriticScore = ({ score }: Props) => {
-  let color = score >= 80 ? 'green' : score >= 60 ? 'yellow' : '';
+  let color = score >= 80 ? 'green' : score >= 60 ? 'yellow' : 'gray';
 
   return (
-    <Badge colorScheme={color} fontSize='14px' paddingX={2} borderRadius='4px'>{score}</Badge>
+    <Badge colorScheme={color} fontSize='14px' paddingX={2} borderRadius='4px'>{score || "N/A"}</Badge>
   );
 }
 
